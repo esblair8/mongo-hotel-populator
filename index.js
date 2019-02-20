3cd94a23fcee246444fad75761e680037a4e8457
Evan Blair <evan.blair@bt.com>
2019-02-20 12:26:32 +0000

    removed config from source

--- index.js
+++ index.js
@@ -3,30 +3,25 @@ const hotelService = require("./hotelService");
 const Hotel = require("./hotelSchema");
 const createSignature = require("./createSignature");
 
-var util = require("util");
-
 mongoose.Promise = global.Promise;
 
-const connection = "";
-
-const params = {
-  fields: "all",
-  language: "ENG",
-  from: 1,
-  to: 5
-};
+const connection = ""
+const from = 4001
+const to = 5000
+const queryString = `/hotels?fields=all&language=ENG&from=${from}&to=${to}`;
+console.log("queryString", queryString);
 
 mongoose
   .connect(connection, { useNewUrlParser: true })
   .then(() => {
     hotelService.defaults.headers["X-Signature"] = createSignature();
-
     hotelService
-      .get("/hotels", params)
+      .get(queryString)
       .then(results => {
-        console.log("results", results);
         results.data.hotels.forEach(hotel => {
-          console.log("hotel", hotel);
+          console.log("==== hotel =====");
+          // console.log(JSON.stringify(hotel));
+          console.log("name", hotel.name, "id", hotel.code);
           new Hotel(hotel).save().then(console.log("===== saved ====="));
         });
         console.log("DONE!");
